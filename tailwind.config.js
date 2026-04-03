5ad7bd86a2aabb3a473afcb0cb4a8ca533b7b75d
MEHMET TOKYAY <mettokyay@gmail.com>
2026-04-03 16:53:07 +0300

    fix-2-fix

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,31 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
-    './index.html',
-    './src/**/*.{js,jsx,ts,tsx}',
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {
-      colors: {
-        amber: {
-          50: '#fef5e7',
-          100: '#fde6b0',
-          200: '#fdd779',
-          300: '#fcc842',
-          400: '#fbb904',
-          500: '#e5a803',
-          600: '#c99502',
-          700: '#a67d02',
-          800: '#846502',
-          900: '#624d01',
-        },
-      },
-      fontFamily: {
-        sans: ['Inter', 'system-ui', 'sans-serif'],
-      },
-    },
+    extend: {},
   },
-  plugins: [
-    require('@tailwindcss/forms'),
-  ],
-}
+  plugins: [],
+};
